1eceaf7896f663cd17f431a88fef8a68ad2c2689
monishs777 <monishdhanalakshmi@gmail.com>
2026-09-17 20:59:16 +0530

    Delete backend directory

--- backend/students/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets, filters
-from .models import Student
-from .serializers import StudentSerializer
-
-class StudentViewSet(viewsets.ModelViewSet):
-    queryset = Student.objects.all().order_by('-id')
-    serializer_class = StudentSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['first_name', 'last_name', 'email', 'course']
